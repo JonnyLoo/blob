e5b51d579c26c93ece32267f77c7c0ddb4e89377
Ashley Teoh <atq203@nyu.edu>
2018-03-14 09:59:54 -0400

    more blobby stuff

--- sketch.js
+++ sketch.js
@@ -1,13 +1,28 @@
+// python 2 server, run `python -m SimpleHTTPServer`
+// python 3 server, run `python -m http.server`
+
 var myCanvas;
 var points = 0;
 var player;
 
+// array to store blobs
+var blobs = [];
+
+// direction
+var HORIZONTAL = "horizontal";
+var VERTICAL = "vertical";
+
 function setup() {
-	myCanvas = createCanvas(800, 800);
+	myCanvas = createCanvas(500, 500);
 	myCanvas.parent("game-container");
 	background(0);
 
 	player = new Player(250, 250);
+
+	for (var i = 0; i < 10; i++) {
+		var blob = new Blob();
+		blobs.push(blob);
+	}
 }
 function preload() {}
 
@@ -15,6 +30,12 @@ function draw() {
 	background(0);
 	player.update();
 	player.display();
+
+  for (var i = 0; i < 10; i++) {
+    blob = blobgs[i];
+    blob.update();
+    blob.display();
+  }
 }
 
 class Player {
@@ -22,6 +43,7 @@ class Player {
 		this.xPos = x;
 		this.yPos = y;
 		this.state = 0; //if lost or not
+		this.size = 5;
 	}
 
 	display() {
@@ -46,10 +68,53 @@ class Player {
 	}
 
 	detectHit(x, y) {
-    // tell the main program that a hit occurred
-    // if not close - not a hit
-		return (dist(x, y, this.xPos, this.yPos) < 50)
+		// tell the main program that a hit occurred
+		// if not close - not a hit
+		return dist(x, y, this.xPos, this.yPos) < 50;
 	}
 }
 
-class Blobs {}
+class Blob {
+	constructor() {
+		this.color = color("red");
+		this.direction = random(0, 1) == 1 ? HORIZONTAL : VERTICAL;
+    this.xPos = getInitialXPos();
+    this.yPos = getInitialYPos():
+    this.xDir = getInitialXDir();
+    this.yDir = getInitialYDir();
+		this.speed = random(15, 50);
+	}
+
+  getInitialXDir() {
+    if (this.direction == HORIZONTAL) {
+    } else {
+      return
+    }
+  }
+
+  getInitialYDir() {
+
+  }
+
+  getInitialXPos() {
+
+  }
+
+  getInitialYPos() {
+
+  }
+
+	display() {
+		imageMode(CENTER);
+		fill(255, 255, 255);
+		ellipse(this.xPos, this.yPos, 20, 20);
+	}
+
+  update() {
+    if (this.direction == HORIZONTAL) {
+      // update x speed
+    } else {
+
+    }
+  }
+}
